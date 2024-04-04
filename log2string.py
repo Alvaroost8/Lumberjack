1e5623acd877d1045ab622e9d881b95ffd26d20c
marinocom <132488376+marinocom@users.noreply.github.com>
2024-04-04 20:05:51 +0200

    log2string

---
+++ log2string.py
@@ -0,0 +1,13 @@
+
+def read_log_file(log_file_path):
+    log_list = []
+    with open(log_file_path, 'r') as file:
+        for line in file:
+            if line.startswith('www.sitges') or line.startswith('sitges'):
+                log_list.append(line.strip())
+    return log_list
+
+# Example TEST
+file_path = ''
+log_strings = read_log_file(file_path)
+print(log_strings)
