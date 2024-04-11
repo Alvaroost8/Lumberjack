9c7e9c59490316e5daa6ef3ab8470fbd7e898437
Alvaroost8 <162721622+Alvaroost8@users.noreply.github.com>
2024-04-11 19:23:03 +0200

    Update log2string.py

--- Functions/log2string.py
+++ Functions/log2string.py
@@ -2,11 +2,5 @@ def read_log_file(log_file_path):
     log_list = []
     with open(log_file_path, 'r') as file:
         for line in file:
-            if line.startswith('www.sitges') or line.startswith('sitges') or line.startswith('www.bestsitges') or line.startswith('bestsitges'):
-                log_list.append(line.strip())
+          log_list.append(line.strip())
     return log_list
-
-# Example TEST
-file_path = ""
-log_strings = read_log_file(file_path)
-print(log_strings)
