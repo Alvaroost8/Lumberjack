e16bc7f4595bdb829372239841917bc44cc8d25c
marinocom <132488376+marinocom@users.noreply.github.com>
2024-04-11 20:17:05 +0200

    Update log2dict.py

--- Functions/log2dict.py
+++ Functions/log2dict.py
@@ -1,5 +1,13 @@
-# Convert log to dictionary
-def log_to_dict(log):
-    ip, country, coords, log_time, instruction, url, response, response_weight, browser=extract_features(log)
-    log_dict={"ip":ip, "country":country, "coords":coords, "log_time":log_time, "instruction":instruction, "url":url, "response":response, "response_weight":response_weight, "browser":browser}
+# Convert log to dictionary, with the ip as the key and the logs as the value
+def log_to_dict(logs, ip_list):
+    logs_ip_dict={}
+    for ip in ip_list:
+        for log in logs:
+            if ip in log:
+                ip, country, coords, log_time, instruction, url, response, response_weight, browser=extract_features(log)
+                log_dict={"ip":ip, "country":country, "coords":coords, "log_time":log_time, "instruction":instruction, "url":url, "response":response, "response_weight":response_weight, "browser":browser}
+                if ip in logs_ip_dict:
+                    logs_ip_dict[ip].append(log_dict)
+                else:
+                    logs_ip_dict[ip]=[log_dict]
     return log_dict
