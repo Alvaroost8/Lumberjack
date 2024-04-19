7012860c4e8bd7b007c89abae13930041c418473
Alvaroost8 <162721622+Alvaroost8@users.noreply.github.com>
2024-04-19 11:19:26 +0200

    Update log2dict.py

--- Functions/log2dict.py
+++ Functions/log2dict.py
@@ -1,13 +1,24 @@
-# Convert log to dictionary, with the ip as the key and the logs as the value
 def log_to_dict(logs, ip_list):
+    i=1
     logs_ip_dict={}
-    for ip in ip_list:
-        for log in logs:
-            if ip in log:
-                ip, country, coords, log_time, instruction, url, response, response_weight, browser=extract_features(log)
-                log_dict={"ip":ip, "country":country, "coords":coords, "log_time":log_time, "instruction":instruction, "url":url, "response":response, "response_weight":response_weight, "browser":browser}
-                if ip in logs_ip_dict:
-                    logs_ip_dict[ip].append(log_dict)
-                else:
-                    logs_ip_dict[ip]=[log_dict]
-    return log_dict
+    time_zones={}
+    countries={}
+    coordinates={}
+    for log in logs:
+        ip, time, instruction, url, response, response_weight, browser=log_segmentation(log)
+        if ip not in logs_ip_dict:
+            country, coords, time_zone=geolocalizate_ip(ip)
+            log_time=calculate_local_time(time, time_zone)
+            time_zones[ip]=time_zone
+            countries[ip]=country
+            coordinates[ip]=coords
+            print(i)
+            i+=1
+        else:
+            log_time=calculate_local_time(time, time_zones[ip])
+        log_dict={"country":countries[ip], "coords":coordinates[ip], "log_time":log_time, "instruction":instruction, "url":url, "response":response, "response_weight":response_weight, "browser":browser}
+        if ip in logs_ip_dict:
+            logs_ip_dict[ip].append(log_dict)
+        else:
+            logs_ip_dict[ip]=[log_dict]
+    return logs_ip_dict
