6ffa1463322342bda4a7abc5eb71fc71460c9ca2
Alvaroost8 <162721622+Alvaroost8@users.noreply.github.com>
2024-05-03 16:32:26 +0200

    Delete Functions/featureExtraction.py

--- Functions/featureExtraction.py
+++
@@ -1,6 +0,0 @@
-# Extract features from log
-def extract_features(log): 
-    ip, time, instruction, url, response, response_weight, browser=log_segmentation(log)
-    country, coords, time_zone=geolocalizate_ip(ip)
-    log_time=calculate_local_time(time, time_zone)
-    return ip, country, coords, log_time, instruction, url, response, response_weight, browser
